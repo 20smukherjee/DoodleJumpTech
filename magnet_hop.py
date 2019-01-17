d6b0f206c640fd7e7e58344a1624485af5aaf9d8
20smukherjee <45695642+20smukherjee@users.noreply.github.com>
2019-01-17 14:36:14 -0500

    Update magnet_hop.py

--- magnet_hop.py
+++ magnet_hop.py
@@ -1,134 +1,148 @@
 from pygame import *
 import random
-import sys
-#import necessary modules
 
-interface_x = 1080
-interface_y = 1920
-#define how large game will be
+interface_x = 400
+interface_y = 450
 
 init()
-interface = display.set_mode((interface_x, interface_y))
-score = time.Clock()
-display.set_caption('Magnet hop!')
- 
-class MagnetJump:
-    def __init__(self):      
-        self.whatever = image.load('RBF.png')
-        #add more states
+window = display.set_mode((interface_x, interface_y))
+display.set_caption('Magnet Hop')
+clock = time.Clock()
+
+
+class Magnet_Man:
+    def __init__(self):       
+        self.exist = image.load('RBF.png')
         self.reset()
-#defines graphical states during game       
- 
-    def reset(self):
+        
+
+    def reset(self): #Sets initial conditions
         self.velocity_x = 0
         self.velocity_y = 0
-        self.x_acceleration = .5
-        self.y_acceleration = -1
-        self.jump_vy = 20
-        self.max_velocity_x = 3
-        self.max_velocity_y = 20
- 
-        self.posx = (interface_x - self.width) / 2
-        self.posy = (interface_y - self.height)
- #defines intial physics and positions to reset to
- 
+        self.max_velocity_x = 5
+        self.max_velocity_y = 15
+        self.x_acceleration = 0.5
+        self.jump_velocity = 15
+
+        scale = 7
+        self.width, self.height = 7 * scale, 12 * scale
+        self.scale = scale
+
+        self.x = (interface_x - self.width) / 2
+        self.y = interface_y - self.height
+
+
     def update(self,p):
-        self.physics(p)
         self.side_control()
+        self.physics(p)
         self.move()
         self.show()
- 
-        self.posx += self.velocity_x
-        self.posy -= self.velocity_y
- 
-        return (self.img, (self.posx, self.posy, self.width, self.height))
- 
-    def physics(self, game):
-    #defines gravity and how velocity and position will change
+        self.x += self.velocity_x
+        self.y -= self.velocity_y
+
+        return (self.img, (self.x, self.y, self.width, self.height))
+
+    def physics(self, p):
+
         on = False
-        #defines variable for true and false
-        for color, rect in game:
+        
+        for color, rect in p:
             x,y,w,h = rect
- 
+
             #X range
-            if self.posx + self.width / 2 > x and self.posx - self.width / 2 < x + w:
+            if self.x + self.width / 2 > x and self.x - self.width / 2 < x + w:
                 #Y range
-                if self.posy + self.height >= y and self.posy + self.height <= y + h:
- 
+                if self.y + self.height >= y and self.y + self.height <= y + h:
+
                     if self.velocity_y < 0:
                         on = True
- 
-        if not on and not self.posy >= interface_y - self.height:
+
+        if not on and not self.y >= interface_y - self.height:
             self.velocity_y -= 0.5
         elif on:
-            self.speed_y = self.jump_velocity
+            self.velocity_y = self.jump_velocity
         else:
-            self.posy = interface_y - self.height
+            self.y = interface_y - self.height
             self.velocity_x = 0
             self.velocity_y = 0
-            if self.posx != (interface_x - self.width) / 2:
-                if self.posx > (interface_x - self.width) / 2:
-                    self.posx = max((interface_x - self.width) / 2, self.posx - 6)
+            if self.x != (interface_x - self.width) / 2:
+                if self.x > (interface_x - self.width) / 2:
+                    self.x = max((interface_x - self.width) / 2, self.x - 6)
                 else:
-                    self.posx = min((interface_x - self.width) / 2, self.posx + 6)
-           
+                    self.x = min((interface_x - self.width) / 2, self.x + 6)
+            
             else:
                 keys = key.get_pressed()
                 if keys[K_SPACE]:
-                    self.velocity_y = self.jump_speed
+                    self.velocity_y = self.jump_velocity
                     
+    def side_control(self):
+        if self.x + self.width < 0:
+            self.x = interface_x - self.scale
+        if self.x > interface_x:
+            self.x = -self.width
+    def show(self): #Creates image
+        self.img = self.exist
+
+        
+    def slowdown(self): 
+        if self.velocity_x < 0: self.velocity_x = min(0, self.velocity_x + self.x_acceleration / 6)
+        if self.velocity_x > 0: self.velocity_x = max(0, self.velocity_x - self.x_acceleration / 6)
+
     def move(self):
-     #Moves the character
-        while True:
-            for e in pygame.event.get():
-                if e.type == pygame.QUIT:
-                    sys.exit()
-                if e.type == pygame.KEYDOWN:
-                    if pygame.key.name(e.key) == 'up':
-                        self.velocity_y -= self.acceleration_y
-                    if pygame.key.name(e.key) == 'left':
-                        self.velocity_x -= self.acceleration_x
-                    if pygame.key.name(e.key) == 'right':
-                        self.velocity_x += self.acceleration_x
-       
-platform_spacing = 100
- 
+        keys = key.get_pressed()
+        if not self.y >= interface_y - self.height:
+
+            if keys[K_LEFT] and keys[K_RIGHT]: self.slowdown()
+            elif keys[K_LEFT]: self.velocity_x -= self.x_acceleration
+            elif keys[K_RIGHT]: self.velocity_x += self.x_acceleration
+            else: self.slowdown()
+
+            self.velocity_x = max(-self.max_velocity_x, min(self.max_velocity_x, self.velocity_x))
+            self.velocity_y = max(-self.max_velocity_y, min(self.max_velocity_y, self.velocity_y))
+
+
+platform_spacing = 60
+
 class Platform_Manager:
     def __init__(self):
         self.platforms = []
         self.spawns = 0
-        self.start_spawn = interface_y
- 
-        scale = 3
-        self.width, self.height = 24 * scale, 6 * scale
-        def update(self):
-            self.generator()
-            return self.manage()
-    def generator(self):
-        if interface_y - info['interface_y'] > self.spawns * platform_spacing:
-            self.generate()
+        self.start_spawn = interface_y+10
 
-    def generate(self):
+        scale = 2.5
+        self.width, self.height = 24 * scale, 6 * scale
+
+    def update(self):
+        self.spawner()
+        return self.manage()
+
+        
+        
+    def spawner(self):
+        if interface_y - info['screen_y'] > self.spawns * platform_spacing:
+            self.spawn()
+        
+    def spawn(self):
         y = self.start_spawn - self.spawns * platform_spacing
         x = random.randint(-self.width, interface_x)
-
+        
         self.platforms.append(Platform(x,y,random.choice([1,-1])))
         self.spawns += 1
 
-
+        
     def manage(self):
-        a = []
+        u = []
         b = []
         for i in self.platforms:
-            i.move()
-            i.change_direction()
             b.append(i.show())
 
             if i.on_screen():
-                a.append(i)
-
-        self.platforms = a
+                u.append(i)
+            
+        self.platforms = u
         return b
+    
 
 
 class Platform:
@@ -136,30 +150,19 @@ class Platform:
         self.x = x
         self.y = y
         self.direction = direction
-        self.speed = 2
         self.color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
         scale = 3
         self.width, self.height = 24 * scale, 6 * scale
 
-    def move(self):
-        self.x += self.speed * self.direction
-        self.change_direction()
-
-    def change_direction(self):
-        if self.x <= 0:
-            self.direction = 1
-        if self.x + self.width >= window_x:
-            self.direction = -1
-
     def on_screen(self):
-        if self.y > info['screen_y'] + window_y:
+        if self.y > info['screen_y'] + interface_y:
             return False
         return True
 
     def show(self):
         return ((0,0,0), (self.x, self.y, self.width, self.height))
 
-def random_color(l,h):
+def random_colour(l,h):
     return (random.randint(l,h),random.randint(l,h),random.randint(l,h))
 
 def blit_images(x):
@@ -176,18 +179,18 @@ def event_loop():
 
 f = font.SysFont('', 50)
 def show_score(score, pos):
-    message = f.render(str(round(score)), True, (100,100,100))
+    message = f.render(str(round(score)), True, (10,50,100))
     rect = message.get_rect()
 
     if pos == 0:
-        x = window_x - rect.width - 10
+        x = interface_x - rect.width - 10
     else:
         x = 10
     y = rect.height + 10
         
     window.blit(message, (x, y))   
         
-
+#Stores the scores and where they go
 info = {
     'screen_y': 0,
     'score': 0,
@@ -195,5 +198,41 @@ info = {
     }
 
 
-MagnetJump = MagnetJump()
+Magnet_man = Magnet_Man()
 platform_manager = Platform_Manager()
+
+while True:
+    #MATH THINGS
+
+    event_loop()
+
+    platform_blit = platform_manager.update()
+    Magnet_blit = Magnet_man.update(platform_blit)
+    info['screen_y'] = min(min(0,Magnet_blit[1][1] - interface_y*0.4),info['screen_y'])
+    info['score'] = (-Magnet_blit[1][1] + 400)/50
+
+    if Magnet_blit[1][1] - 470 > info['screen_y']:
+        info['score'] = 0
+        info['screen_y'] = 0
+        Magnet_man = Magnet_Man()
+        platform_manager = Platform_Manager()
+
+    clock.tick(60)
+
+    #DISPLAY THINGS
+    window.fill((255,255,255))
+
+    blit_images([Magnet_blit])
+    
+    for x in platform_blit:
+        i = list(x)
+        i[1] = list(i[1])
+        i[1][1] -= info['screen_y']
+        draw.rect(window, i[0], i[1])
+
+    info['high_score'] = max(info['high_score'], info['score'])
+
+    show_score(info['score'],1)
+    show_score(info['high_score'],0)
+
+    display.update()
